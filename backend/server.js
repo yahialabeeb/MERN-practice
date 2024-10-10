e5caef574f72beb6278163dec9f78d6abd75f211
Yahia Labeeb <yahia.labib@hearst.com>
2024-10-10 14:56:52 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started on port 5000");
+});
